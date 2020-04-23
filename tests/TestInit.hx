e53c18069fdbc22737072466c827af494a72fb81
Juraj Kirchheim <back2dos@googlemail.com>
2020-04-23 10:41:24 +0200

    Add test for #5.

--- tests/TestInit.hx
+++ tests/TestInit.hx
@@ -6,16 +6,19 @@ using Lambda;
 class TestInit extends Base {
   function test() {
     var d = new Dummy('x');
-    
+
     assertEquals('x', d.bar);
     assertEquals(Dummy.value, d.foo);
     assertEquals('baz', d.baz);
-    
+
     d = new Dummy('y', 'zab');
-    
+
     assertEquals('y', d.bar);
     assertEquals(Dummy.value, d.foo);
     assertEquals('zab', d.baz);
+
+    assertTrue(d.issue5[12]);
+    assertFalse(d.issue5[12]);
   }
 }
 
@@ -24,4 +27,5 @@ class TestInit extends Base {
   public var foo:Array<Int> = Dummy.value;
   public var bar:String = _;
   public var baz = @byDefault 'baz';
+  public var issue5 = [12 => true, 14 => false];
 }
